bceb4a2b71ee4b35d8c3265be8ccdd9792ecf81b
Henrique <henrique.x1@hotmail.com>
2023-08-07 09:47:03 -0300

    Arquivos finais

---
+++ MInhaAppFuncional/src/AppMvcFuncional/Models/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcFuncional.Models {
+    public class Aluno {
+        //Identificação
+        [Key]
+        public int Id { get; set; }
+
+        //Nome do aluno
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+        public string? Nome { get; set; }
+
+        //Data de nascimento
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} esta em formato incorreto")]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        //Email
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no maximo {1} caracteres")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "O campo {0} está em formato invalido.")]
+        //Outra opcao para validacao de formato de email:
+        //[EmailAddress(ErrorMessage = "O campo {0} esta em formato incorreto")]
+        public string? Email { get; set; }
+
+        //Email de confirmação
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [Display(Name = "Confirme o email")]
+        [Compare("Email", ErrorMessage = "Os emails informados nao sao identicos")]
+        [NotMapped]
+        public string? EmailConfirmacao { get; set; }
+
+        //Avaliação aluno
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve estar entre {1} e {2}")]
+        public int Avaliacao { get; set; }
+
+        //Ativo
+        public bool Ativo { get; set; }
+    }
+}
---
+++ PrimeiraApp/Controllers/TesteEFController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers {
+    public class TesteEFController : Controller {
+
+        public AppDbContext Db { get; set; }
+
+        public TesteEFController(AppDbContext db) {
+            Db = db;
+        }
+
+        public IActionResult Index() {
+
+            var aluno = new Aluno() {
+                Nome = "Henrique",
+                Email = "henrique@email.com",
+                DataNascimento = new DateTime(1996,11,26),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            //Db.Alunos.Add(aluno);
+            //Db.SaveChanges();
+
+            var alunoChange = Db.Alunos.Where(a => a.Nome.Contains("Henrique")).FirstOrDefault();
+
+            //alunoChange.Nome = "Henrique Santos";
+
+            //Db.Alunos.Update(alunoChange);
+            //Db.SaveChanges();
+
+            Db.Alunos.Remove(alunoChange);
+            Db.SaveChanges();
+
+            return Content("");
+        }
+    }
+}
